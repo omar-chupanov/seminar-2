29ff9a40aa5f8c35a85fb1305a28d3a24a8b13aa
Omar <omar-chupanov@mail.ru>
2023-04-01 15:10:35 +0300

    задача 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.Write("Укажите трехзначное число >");
+string value;
+value = Console.ReadLine(); 
+int number = Convert.ToInt32(value); 
+if(number<1000)
+{
+    if(number>99)
+    {   
+        int penultimate = number / 10 %10;
+        System.Console.Write($"Вторая цифра числа {number} является {penultimate}" );
+    }  
+    else
+    {
+        System.Console.Write($"{number} НЕ является трехзначным числом!");
+    }  
+}
+else
+{
+    System.Console.Write($"{number} НЕ является трехзначным числом!");
+}
+
+
+
+
+
